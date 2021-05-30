63a2c8e64350a12fa0294866542c7f66d01f0d06
sofast <kevinkevin556>
2021-05-30 22:04:01 +0800

    Add app_structure folder.

--- __init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Mar 20 18:46:24 2021
-
-@author: acer
-"""
-
-
---
+++ app_structure/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 20 18:46:24 2021
+
+@author: acer
+"""
+
+
