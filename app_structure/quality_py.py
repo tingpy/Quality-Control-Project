28f91aaafd9f9459ca239a34cf42d5ad1da49711
tingpy <53030049+tingpy@users.noreply.github.com>
2021-06-01 19:41:51 +0800

    Add files via upload

---
+++ app_structure/quality_py.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 23 23:32:09 2021
+
+@author: acer
+"""
+
+import function
+
+#### load the original data
+spec, deal, agent, customer = function.import_data()
